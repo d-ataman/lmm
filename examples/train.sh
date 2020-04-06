ee3100355f15e13c277577ebacac1754f0dfd169
Duygu Ataman <ataman@glados.ifi.uzh.ch>
2020-04-06 16:25:38 +0200

    updated example scripts

--- examples/train.sh
+++ examples/train.sh
@@ -5,6 +5,7 @@ exp_dir=$HOME/experiments/en-tr
 opennmt=$HOME/lmm
 src=en
 tgt=tr
+num_epochs=200
 
-$python $opennmt/train.py -data $exp_dir/iwslt -epochs 150 -word_vec_size 512 -enc_layers 1 -dec_layers 1 -seed 1234 -rnn_size 512 -rnn_type GRU -encoder_type birnn -decoder_type charrnn -tgt_data_type characters -optim adam -learning_rate 0.0003 -learning_rate_decay 0.9 -dropout 0.2 -start_decay_at 20 -start_checkpoint_at 5 -save_model $exp_dir/model -gpu 1 -gpuid 1 -max_grad_norm 1 > log.out
+$python $opennmt/train.py -data $exp_dir/iwslt -epochs $num_epochs -word_vec_size 512 -enc_layers 1 -dec_layers 1 -seed 1234 -rnn_size 512 -rnn_type GRU -encoder_type birnn -decoder_type charrnn -tgt_data_type characters -optim adam -learning_rate 0.0003 -learning_rate_decay 0.9 -dropout 0.2 -start_decay_at $num_epochs -start_checkpoint_at 5 -save_model $exp_dir/model -gpu 1 -gpuid 1 -max_grad_norm 1 > log.out
 
